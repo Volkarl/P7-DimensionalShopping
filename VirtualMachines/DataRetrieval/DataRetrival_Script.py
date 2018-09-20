815babdc4ab3d7aca27997e9447724c0a168cd45
martin karkov <thinhar2@gmail.com>
2018-09-20 15:26:54 +0200

    small changes

--- VirtualMachines/DataRetrieval/DataRetrival_Script.py
+++ VirtualMachines/DataRetrieval/DataRetrival_Script.py
@@ -6,8 +6,8 @@ url = sys.argv[1]
 def valuta(x):
     return {
         'dk'   : 'DKK ',
-        'us'   : '$ ',
-        'uk'   : '£ ',
+        'us'   : '$',
+        'uk'   : '£',
         'euro' : 'euro '
     }.get(x, 'DKK')    
 
@@ -16,8 +16,8 @@ def valuta(x):
 driver = webdriver.Chrome()  # Optional argument, if not specified will search path.
 driver.get(url)
 #time.sleep(5) # Let the user actually see something! why no need for waiting for callback
-price = driver.find_element_by_xpath('//*[@id="flightModuleList"]').text.split('\n')[0].split(' ')[10]
-print(price)
+#price = driver.find_element_by_xpath('//*[@id="flightModuleList"]').text.split('\n')[0].split(' ')[10]
+#print(price)
 price = driver.find_element_by_xpath('//*[@id="flightModuleList"]').text.split('\n')[0].split(valuta(sys.argv[2]))[1]
 print(price)
 
