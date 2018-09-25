a0bb7d0bd5ef08248ba3ea3ba6d9dced429f511b
martin karkov <thinhar2@gmail.com>
2018-09-25 09:15:02 +0000

    small fixed, evnironment is fixed, only nede zip

--- VirtualMachines/DataRetrieval/AutomatedSetup/setup.ps1
+++ VirtualMachines/DataRetrieval/AutomatedSetup/setup.ps1
@@ -7,23 +7,25 @@ choco install python -y
 choco install pip -y
 
 #use pip(package manager) to install selenium
-python -m pip -U selenium
+python -m pip install selenium
 
 
-$LocalTempDir = $env:TEMP; $ChromeInstaller = "ChromeInstaller.exe"; (new-object    System.Net.WebClient).DownloadFile('http://dl.google.com/chrome/install/375.126/chrome_installer.exe', "$LocalTempDir\$ChromeInstaller"); & "$LocalTempDir\$ChromeInstaller" /silent /install; $Process2Monitor =  "ChromeInstaller"; Do { $ProcessesFound = Get-Process | ?{$Process2Monitor -contains $_.Name} | Select-Object -ExpandProperty Name; If ($ProcessesFound) { "Still running: $($ProcessesFound -join ', ')" | Write-Host; Start-Sleep -Seconds 2 } else { rm "$LocalTempDir\$ChromeInstaller" -ErrorAction SilentlyContinue -Verbose } } Until (!$ProcessesFound)
+#$LocalTempDir = $env:TEMP; $ChromeInstaller = "ChromeInstaller.exe"; (new-object    System.Net.WebClient).DownloadFile('http://dl.google.com/chrome/install/375.126/chrome_installer.exe', "$LocalTempDir\$ChromeInstaller"); & "$LocalTempDir\$ChromeInstaller" /silent /install; $Process2Monitor =  "ChromeInstaller"; Do { $ProcessesFound = Get-Process | ?{$Process2Monitor -contains $_.Name} | Select-Object -ExpandProperty Name; If ($ProcessesFound) { "Still running: $($ProcessesFound -join ', ')" | Write-Host; Start-Sleep -Seconds 2 } else { rm "$LocalTempDir\$ChromeInstaller" -ErrorAction SilentlyContinue -Verbose } } Until (!$ProcessesFound)
 
-bitsadmin /create thisissomejobname 
-bitsadmin /addfile thisissomejobname https://chromedriver.storage.googleapis.com/2.42/chromedriver_win32.zip C:\chromedriver.zip
-bitsadmin /complete thisissomejobname 
+Start-BitsTransfer -Source https://chromedriver.storage.googleapis.com/2.42/chromedriver_win32.zip -Destination "C:\chromedriver.zip"
 
 #unzip chromedriver
-New-Item -ItemType directory -Path C:\webdriver
+New-Item -ItemType directory -force -Path C:\webdriver
+
+$sourceFile = "C:\chromedriver.zip"
 
 Add-Type -Assembly System.IO.Compression.FileSystem
 $zip = [IO.Compression.ZipFile]::OpenRead($sourceFile)
-$zip.Entries | where {$_.Name -like '*.update'} | foreach {[System.IO.Compression.ZipFileExtensions]::ExtractToFile(C:\chromedriver.zip, C:\webdriver\chromedriver.exe, $true)}
+$zip.Entries | where {$_.Name -like '*.update'} | foreach {[System.IO.Compression.ZipFileExtensions]::ExtractToFile($_, "C:\webdriver\chromedriver.exe", $true)}
 $zip.Dispose()
-
+#$zip = [IO.Compression.ZipFile]::OpenRead("C:\chromedriver.zip")
+#$zip.Entries | where {$_.Name -like '*.update'} | foreach {[System.IO.Compression.ZipFileExtensions]::ExtractToFile(${C:\chromedriver.zip}, "C:\webdriver\chromedriver.exe", $true)}
+#$zip.Dispose()
+ 
 #do path for chromedriver
-set PATH=%PATH%;C:\webdriver
-
+SETX path $Env:Path';C:\webdriver' /m
