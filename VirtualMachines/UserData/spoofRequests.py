6af2d04d157c058b7ded97e9492c5859c396591b
Unknown <Karlzone0@gmail.com>
2018-10-26 16:20:33 +0200

    Now randomly switches user-agents

--- VirtualMachines/UserData/spoofRequests.py
+++ VirtualMachines/UserData/spoofRequests.py
@@ -1,7 +1,7 @@
-from lxml import html
-import urllib.request
+#from lxml import html
+#import urllib.request
 
-url = "https://www.whatsmyua.info"
+#url = "https://www.whatsmyua.info"
 
 #page = requests.get(url)
 #tree = html.fromstring(page.content)
@@ -30,3 +30,36 @@ url = "https://www.whatsmyua.info"
 #        ## can add authentication here 
 #    else:
 #        print('http error',e)
+
+
+
+
+
+
+
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from fake_useragent import UserAgent
+
+url = "https://www.whatsmyua.info"
+
+print(url)
+
+# Options
+options = Options()
+options.headless = True
+
+# User Agent
+ua = UserAgent()
+userAgent = ua.random
+options.add_argument(f'user-agent={userAgent}')
+#options.add_argument('user_agent=Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.101 Safari/537.36')
+
+driver = webdriver.Chrome(options = options)  # Optional argument, if not specified it will search the path environment var
+driver.get(url)
+uainfo = driver.find_element_by_xpath('//*[@id="rawUa"]').text
+uainfo = uainfo.replace('rawUa: ','')
+print(uainfo)
+
+driver.quit()
