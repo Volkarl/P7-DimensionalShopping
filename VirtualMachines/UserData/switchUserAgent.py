34c3746f91dbb409b5dcadf56dc9f22ac6079671
Unknown <Karlzone0@gmail.com>
2018-10-30 11:17:45 +0100

    UserAgent spoofing done, can also query multiple vendors

---
+++ VirtualMachines/UserData/switchUserAgent.py
@@ -0,0 +1,13 @@
+
+# Not all combinations are allowed, so we have selected a few
+# These combinations were found using website: https://developers.whatismybrowser.com/useragents/explore/
+# We used the top result for each of the operating system that is the highest version (within the top 10) and the correct device type
+def getUserAgentString(x):
+    return {
+    	'PcWindowsFirefox'		: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36',
+        'PcLinuxChrome'   		: 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36',
+        'PhoneAndroidChrome'	: 'Mozilla/5.0 (Linux; Android 6.0.1; SM-G532G Build/MMB29T) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.83 Mobile Safari/537.36',
+        'PhoneIOSSafari'		: 'Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1',
+        'PcMacosxSafari'		: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/603.3.8 (KHTML, like Gecko) Version/10.1.2 Safari/603.3.8'
+    }.get(x, '')    
+
