0b1e613bf9f63e9f46d0589878c7a49a85abfbc2
Unknown <Karlzone0@gmail.com>
2018-11-01 15:04:32 +0100

    Switched webdriver to firefox

--- VirtualMachines/UserData/query.py
+++ VirtualMachines/UserData/query.py
@@ -1,6 +1,6 @@
 import time, sys
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.firefox.options import Options
 from switchUserAgent import getUserAgentString
 from crawler import crawlUrl
 
@@ -15,14 +15,14 @@ print(f'Configuring user agent: {userAgent}')
 options.add_argument(f'user-agent={userAgent}')
 
 # Initialize
-driver = webdriver.Chrome(options = options)  # Optional argument, if not specified it will search the path environment var
+driver = webdriver.Firefox(options = options)  # Optional argument, if not specified it will search the path environment var
 
 # Cookies
 deleteCookies = sys.argv[3]
 if deleteCookies == "True" : 
 	print("Deleting Cookies")
 	driver.delete_all_cookies()
-#### Is this necessary? I feel like we're not using any cookies anyway, since we open a new chrome webdriver each request
+#### Is this necessary? I feel like we're not using any cookies anyway, since we open a new webdriver each request
 
 # Get website
 driver.get(url)
--- VirtualMachines/UserData/switchUserAgent.py
+++ VirtualMachines/UserData/switchUserAgent.py
@@ -4,7 +4,7 @@
 # We used the top result for each of the operating system that is the highest version (within the top 10) and the correct device type
 def getUserAgentString(x):
     return {
-    	'PcWindowsFirefox'		: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36',
+    	'PcWindowsChrome'		: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36',
         'PcLinuxChrome'   		: 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36',
         'PhoneAndroidChrome'	: 'Mozilla/5.0 (Linux; Android 6.0.1; SM-G532G Build/MMB29T) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.83 Mobile Safari/537.36',
         'PhoneIOSSafari'		: 'Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1',
