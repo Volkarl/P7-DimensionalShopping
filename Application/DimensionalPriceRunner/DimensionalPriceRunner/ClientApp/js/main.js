54ae6207f786613d81a39cb92a0c9b50291f2063
Kim <kimhtxt12@gmail.com>
2018-11-01 11:36:00 +0100

    Prototype designs

--- Application/DimensionalPriceRunner/DimensionalPriceRunner/ClientApp/js/main.js
+++ Application/DimensionalPriceRunner/DimensionalPriceRunner/ClientApp/js/main.js
@@ -4,3 +4,26 @@ import $ from 'jquery';
 
 import 'popper.js';
 import 'bootstrap';
+
+
+//Design 3 Scripts
+$('#myCarousel').carousel({
+    interval: 40000
+});
+
+$('.carousel .item').each(function () {
+    var next = $(this).next();
+    if (!next.length) {
+        next = $(this).siblings(':first');
+    }
+    next.children(':first-child').clone().appendTo($(this));
+
+    if (next.next().length > 0) {
+
+        next.next().children(':first-child').clone().appendTo($(this)).addClass('rightest');
+
+    } else {
+        $(this).siblings(':first').children(':first-child').clone().appendTo($(this));
+
+    }
+});
