8999601cfc18f1cd1ae7a17f9516e84a764c426c
Unknown <Karlzone0@gmail.com>
2018-11-13 17:13:36 +0100

    Trying with sudo

--- Backend/AutomatedSetup/dockerEntrypoint.sh
+++ Backend/AutomatedSetup/dockerEntrypoint.sh
@@ -2,7 +2,7 @@
 
 # Creates and configures TUN/TAP device driver, necessary for creating a connection with openVPN
 # See official documentation at https://www.kernel.org/doc/Documentation/networking/tuntap.txt
-mkdir -p /dev/net 
-mknod /dev/net/tun c 10 200
-chmod 666 /dev/net/tun
+sudo mkdir -p /dev/net 
+sudo mknod /dev/net/tun c 10 200
+sudp chmod 666 /dev/net/tun
 # Changes permissions to allow all users to read and write (not execute)
