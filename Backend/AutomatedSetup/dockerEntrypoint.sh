ec19e51823498c67dfd32fd166d3a91aef456123
Unknown <Karlzone0@gmail.com>
2018-11-13 18:20:07 +0100

    Fixed the problems and added username as var for entrypoint

--- Backend/AutomatedSetup/dockerEntrypoint.sh
+++ Backend/AutomatedSetup/dockerEntrypoint.sh
@@ -1,11 +1,27 @@
 #!/bin/bash
+username=$1
 
 # Creates and configures TUN/TAP device driver, necessary for creating a connection with openVPN
 # See official documentation at https://www.kernel.org/doc/Documentation/networking/tuntap.txt
-sudo mkdir -p /dev/net 
-sudo mknod /dev/net/tun c 10 200
-sudo chmod 666 /dev/net/tun
+mkdir -p /dev/net 
+mknod /dev/net/tun c 10 200
+chmod 666 /dev/net/tun
 # Changes permissions to allow all users to read and write (not execute)
+
+
+su $username
+# Switch to non-root user
+
+
+#############################################
+# Now we are ready to execute the query.py script
+
+# Missing: initialize connection somehow?
 
 /bin/bash 
 # Run terminal, because the container will otherwise exit immedeately
+
+# Build with docker build PATHTODOCKERFILE
+# For now, run the docker container with: docker run -it --cap-add=NET_ADMIN IDOFDOCKERIMAGE
+# Perhaps I can find a way to add --cap-add=NET_ADMIN to this file
+# I may be able to remove -it as well, once I don't want to run it in the terminal (perhaps I can already do it now, who knows)
