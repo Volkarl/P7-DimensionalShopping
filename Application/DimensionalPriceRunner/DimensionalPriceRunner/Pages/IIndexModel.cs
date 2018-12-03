f372e7705407395bce8aad7aecf454827844cb0b
Kim <kimhtxt12@gmail.com>
2018-12-03 16:18:27 +0100

    Dynamic Modal, Postback, Index Page Types

---
+++ Application/DimensionalPriceRunner/DimensionalPriceRunner/Pages/IIndexModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace DimensionalPriceRunner.Pages
+{
+    public interface IIndexModel
+    {
+        IndexModel.Currency ActiveCurrency { get; set; }
+        IEnumerable<IndexModel.Result> Results { get; set; }
+
+        void OnGet();
+    }
+}
--- Application/DimensionalPriceRunner/DimensionalPriceRunner/Pages/Index.cshtml.cs
+++ Application/DimensionalPriceRunner/DimensionalPriceRunner/Pages/Index.cshtml.cs
@@ -1,26 +1,35 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Globalization;
 
 namespace DimensionalPriceRunner.Pages
 {
     public class IndexModel : PageModel
     {
         public IEnumerable<Result> Results { get; set; }
-        public enum Valuta { DKK, USD, EUR }
-        public Valuta ActiveValuta { get; set; }
+        public enum Currency { DKK, USD, EUR }
+        public Currency ActiveCurrency { get; set; }
 
-        public enum Airlines { SAS, BritishAirways }
+
+
+        public enum Airlines { Default, SAS, BritishAirways, test }
 
         // Airline Links should be in 2:1 format
         private static readonly Dictionary<Airlines, string> AirlineDictionary = new Dictionary<Airlines, string>()
         {
             { Airlines.SAS, "https://upload.wikimedia.org/wikipedia/commons/thumb/3/33/Scandinavian_Airlines_logo.svg/1280px-Scandinavian_Airlines_logo.svg.png"},
-            { Airlines.BritishAirways, "https://www.alternativeairlines.com/images/global/airlinelogos/ba_logo.gif"}
+            { Airlines.BritishAirways, "https://www.alternativeairlines.com/images/global/airlinelogos/ba_logo.gif"},
+
+            { Airlines.Default, "http://betlosers.com/images/stories/flexicontent/leagueimages/m_fld34_NoImageAvailable.jpg" } 
+            // tag højde for sporg? http://betlosers.com/images/stories/flexicontent/leagueimages/m_fld34_NoImageAvailable.jpg
+            // https://premium.wpmudev.org/blog/wp-content/uploads/2012/06/no.image_.600x300.png
         };
+
+
+
+
+
 
         public class Ticket
         {
@@ -32,39 +41,31 @@ namespace DimensionalPriceRunner.Pages
                 this.Price = price;
                 this.Airline = airline;
             }
+
         }
 
         public class Result
         {
-            public int ID { get; set; }
+            public string ID { get; set; }
+            public string Name { get; set; }
             public string OS { get; set; }
             public string VPNLocation { get; set; }
             public Ticket Ticket { get; set; }
 
-            public Result(int id, string os, string vpnLoc, Ticket ticket)
+            public Result(string id, string name, string os, string vpnLoc, Ticket ticket)
             {
                 this.ID = id;
+                this.Name = name;
                 this.OS = os;
                 this.VPNLocation = vpnLoc;
                 this.Ticket = ticket;
             }
         }
-
-
+        
 
         public void OnGet()
         {
-            ActiveValuta = Valuta.DKK;
-
-            Result one = new Result(1, "Windows 7", "Denmark", new Ticket(ConvertToValuta(ActiveValuta, 1000), AirlineDictionary[Airlines.SAS]));
-            Result two = new Result(2, "Windows 8", "USA", new Ticket(ConvertToValuta(ActiveValuta, 2000), AirlineDictionary[Airlines.BritishAirways]));
-            Result three = new Result(3, "Windows 10", "Tyskland", new Ticket(ConvertToValuta(ActiveValuta, 3000), AirlineDictionary[Airlines.SAS]));
-            Result four = new Result(4, "iOS", "USA", new Ticket(ConvertToValuta(ActiveValuta, 4000), AirlineDictionary[Airlines.BritishAirways]));
-
-            Results = new List<Result>() { one, two, three, four };
-
-
-
+            ActiveCurrency = Currency.DKK;
 
 
 
@@ -72,22 +73,97 @@ namespace DimensionalPriceRunner.Pages
 
 
         }
+
+
+
+        public void OnPost()
+        {
+            var searchInput = Request.Form["search"];
+            ViewData["search-input"] = searchInput;
+
+            if (searchInput == "test@mail.com")
+            {
+
+
+                Result one = new Result("a" + Guid.NewGuid(),
+                    Airlines.SAS.ToString(),
+                    "Windows 7", "Denmark",
+                    new Ticket(ConvertToCurrency(ActiveCurrency, 1000), AirlineDictionary[Airlines.SAS]));
+
+                Result two = new Result("a" + Guid.NewGuid(),
+                    Airlines.BritishAirways.ToString(),
+                    "Windows 8", "USA",
+                    new Ticket(ConvertToCurrency(ActiveCurrency, 2000), AirlineDictionary[Airlines.BritishAirways]));
+
+                Result three = new Result("a" + Guid.NewGuid(),
+                    Airlines.SAS.ToString(),
+                    "Windows 10", "Tyskland",
+                    new Ticket(ConvertToCurrency(ActiveCurrency, 3000), AirlineDictionary[Airlines.SAS]));
+
+                Result four = new Result("a" + Guid.NewGuid(),
+                    Airlines.BritishAirways.ToString(),
+                    "iOS", "USA",
+                    new Ticket(ConvertToCurrency(ActiveCurrency, 4000), AirlineDictionary[Airlines.BritishAirways]));
+
+
+                Result one2 = new Result("a" + Guid.NewGuid(),
+                    Airlines.SAS.ToString(),
+                    "Windows 7", "Denmark",
+                    new Ticket(ConvertToCurrency(ActiveCurrency, 5000), AirlineDictionary[Airlines.SAS]));
+
+                Result two2 = new Result("a" + Guid.NewGuid(),
+                    Airlines.BritishAirways.ToString(),
+                    "Windows 8", "USA",
+                    new Ticket(ConvertToCurrency(ActiveCurrency, 6000), AirlineDictionary[Airlines.BritishAirways]));
+
+                Result three2 = new Result("a" + Guid.NewGuid(),
+                    Airlines.SAS.ToString(),
+                    "Windows 10", "Tyskland",
+                    new Ticket(ConvertToCurrency(ActiveCurrency, 7000), AirlineDictionary[Airlines.SAS]));
+
+                Result four2 = new Result("a" + Guid.NewGuid(),
+                    Airlines.BritishAirways.ToString(),
+                    "iOS", "USA",
+                    new Ticket(ConvertToCurrency(ActiveCurrency, 8000), AirlineDictionary[Airlines.BritishAirways]));
+
+
+                Results = new List<Result>() {one, two, three, four, one2, two2, three2, four2};
+
+
+
+            }
+            else
+            {
+                
+            }
+
+            
+
+        }
+
+
+
+
+
+
 
         // TODO: Is default Valuta always the same? This method asumes start valuta is always in USD
-        private decimal ConvertToValuta(Valuta valuta, decimal price)
+        private decimal ConvertToCurrency(Currency currency, decimal price)
         {
-            switch (valuta)
+            switch (currency)
             {
-                case Valuta.DKK:
+                case Currency.DKK:
                     return Math.Round(price * 6.61244462m, 2);
-                case Valuta.USD:
+                case Currency.USD:
                     return price;
-                case Valuta.EUR:
+                case Currency.EUR:
                     return Math.Round(price * 0.886194857m, 2);
                 default:
-                    throw new Exception("Valuta not defined");
+                    throw new Exception("Currency not defined");
             }
         }
+
+
 
 
     }
