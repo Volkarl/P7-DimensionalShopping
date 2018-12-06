e3b1b1ae2035b2abe27dffe8d9debd404c3d4715
Unknown <Karlzone0@gmail.com>
2018-12-06 14:50:25 +0100

    Now supply username as argument to entrypoint

--- Backend/nodejs/callQuery.sh
+++ Backend/nodejs/callQuery.sh
@@ -5,7 +5,7 @@ requestedUserAgent=$2
 deleteCookies=$3
 location=$4
 
-echo $url $requestedUserAgent $deleteCookies $location $(cat /etc/ipvanish/email) $(cat /etc/ipvanish/password) $(cat /etc/pcUsername) > /home/$pcUsername/arguments
+echo $url $requestedUserAgent $deleteCookies $location $(cat /etc/ipvanish/email) $(cat /etc/ipvanish/password) $(cat /etc/username) > /home/$pcUsername/arguments
 sudo -u $pcUsername -i /bin/bash - <<-'EOF'
 	~/P7-DimensionalShopping/Backend/query.py $(cat arguments)
 EOF
