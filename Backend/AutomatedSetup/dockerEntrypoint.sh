5756b268cf5d2c3b4791c86351002fc9a412d8f1
Unknown <Karlzone0@gmail.com>
2018-12-06 19:10:28 +0100

    Once again takes username as variable

--- Backend/AutomatedSetup/dockerEntrypoint.sh
+++ Backend/AutomatedSetup/dockerEntrypoint.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-# Takes pcUsername as input from entrypoint, as defined in dockerfile or kubernetes configuration
-pcUsername=$1
-
-echo $pcUsername > /etc/username 
-# Saves pcUsername in file, so that we can read it from the file "callQuery.sh", and know where our openVPN files are placed
-
 # Creates and configures TUN/TAP device driver, necessary for creating a connection with openVPN
 # See official documentation at https://www.kernel.org/doc/Documentation/networking/tuntap.txt
 mkdir -p /dev/net 
@@ -13,4 +7,4 @@ mknod /dev/net/tun c 10 200
 chmod 666 /dev/net/tun
 # Changes permissions to allow all users to read and write (not execute)
 
-node /home/$pcUsername/P7-DimensionalShopping/Backend/nodejs/server.js
+node /home/$(cat /etc/username)/P7-DimensionalShopping/Backend/nodejs/server.js
