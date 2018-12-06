5dbaa39cf4256346336a83afa3ccccd4e1838f72
Unknown <Karlzone0@gmail.com>
2018-12-06 15:47:09 +0100

    Now tests manually once again

--- Backend/AutomatedSetup/dockerEntrypoint.sh
+++ Backend/AutomatedSetup/dockerEntrypoint.sh
@@ -13,4 +13,5 @@ mknod /dev/net/tun c 10 200
 chmod 666 /dev/net/tun
 # Changes permissions to allow all users to read and write (not execute)
 
-node /home/$pcUsername/P7-DimensionalShopping/Backend/nodejs/server.js
+# node /home/$pcUsername/P7-DimensionalShopping/Backend/nodejs/server.js
+/bin/bash
