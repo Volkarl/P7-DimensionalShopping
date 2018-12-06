5a49c03df64752c5aa1ed2c687a3a114153f3502
Unknown <Karlzone0@gmail.com>
2018-12-06 16:16:03 +0100

    Removed bash entrypoint

--- Backend/AutomatedSetup/dockerEntrypoint.sh
+++ Backend/AutomatedSetup/dockerEntrypoint.sh
@@ -13,5 +13,4 @@ mknod /dev/net/tun c 10 200
 chmod 666 /dev/net/tun
 # Changes permissions to allow all users to read and write (not execute)
 
-# node /home/$pcUsername/P7-DimensionalShopping/Backend/nodejs/server.js
-/bin/bash
+node /home/$pcUsername/P7-DimensionalShopping/Backend/nodejs/server.js
