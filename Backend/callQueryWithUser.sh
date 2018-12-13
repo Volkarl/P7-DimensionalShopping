d5ae10ba5d97c9c1dcb624383356ee6e85560e18
Unknown <Karlzone0@gmail.com>
2018-12-13 23:51:43 +0100

    Fiex username error

--- Backend/callQueryWithUser.sh
+++ Backend/callQueryWithUser.sh
@@ -6,7 +6,7 @@ deleteCookies=$3
 
 pcUsername=$(cat /etc/username)
 
-echo $url $requestedUserAgent $deleteCookies > /home/$pcUsername/queryArguments
+echo $url $requestedUserAgent $deleteCookies $pcUsername > /home/$pcUsername/queryArguments
 sudo -u $pcUsername -i /bin/bash - <<-'EOF'
 	~/P7-DimensionalShopping/Backend/query.py $(cat queryArguments)
 EOF
