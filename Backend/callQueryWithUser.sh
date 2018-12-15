10041d2fdb3d18e448691c0113e85057956ce269
martin karkov <thinhar2@gmail.com>
2018-12-15 10:56:06 +0100

    Auto stash before revert of "decodeURIComponent"

--- Backend/callQueryWithUser.sh
+++ Backend/callQueryWithUser.sh
@@ -6,10 +6,7 @@ deleteCookies=$3
 
 pcUsername=$(cat /etc/username)
 
-urldecode() { : "${*//+/ }"; echo -e "${_//%/\\x}"; }
-decodedURL=$(urldecode $url)
-
-echo $decodedURL $requestedUserAgent $deleteCookies $pcUsername > /home/$pcUsername/queryArguments
+echo $url $requestedUserAgent $deleteCookies $pcUsername > /home/$pcUsername/queryArguments
 sudo -u $pcUsername -i /bin/bash - <<-'EOF'
 	~/P7-DimensionalShopping/Backend/query.py $(cat queryArguments)
 EOF
